3bbf9943692c848addbaee40267ff71c2adae25a
Alfred Weidenbladh <alfred.weidenbladh@gmail.com>
2016-02-17 15:21:40 +0100

    Nytt js + css

--- js/battleship.js
+++ js/battleship.js
@@ -1,1 +1,27 @@
 "use strict";
+
+var Battleship = function() {
+	this.changeColor = function(element) {
+		element.style.color = "red";
+	}
+
+	var width = 9;
+	var height = 9;
+	var buttonID = 0;
+	var output = document.getElementById("battleships");
+
+	for(var i = 0; i < height; i++) {
+		for(var j = 0; j < width; j++) {
+			$(output).append("<button id=\"button"+buttonID+"\" onclick=\"changeColor(this)\">&nbsp;</button>");
+			buttonID++;
+		}
+		$(output).append("<br />");
+	}
+
+	
+}
+
+$(function() {
+	//Instantiate
+	var game = new Battleship();
+});
